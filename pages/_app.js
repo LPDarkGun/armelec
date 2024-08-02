aeb2468600786cec59d8a5ee11c37b53cf1e2824
Erik Gaboyan <erikgaboyan@Eriks-Laptop.lan>
2024-08-01 23:38:36 -0700

    stufff

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
-import "@/styles/globals.css"
-import "../i18n"
+import React, { useEffect } from "react"
+import i18n from "../i18n"
 import HeaderComponent from "@/components/Header"
 import FooterComponent from "@/components/Footer"
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    i18n.changeLanguage(detectLanguage())
+  }, [])
+
   return (
     <>
       <HeaderComponent />
@@ -11,4 +15,16 @@ export default function App({ Component, pageProps }) {
       <FooterComponent />
     </>
   )
+}
+
+const detectLanguage = () => {
+  if (typeof window !== "undefined") {
+    const userLanguage = navigator.language || navigator.userLanguage
+    if (userLanguage.includes("hy")) {
+      return "hy"
+    } else if (userLanguage.includes("es")) {
+      return "es"
+    }
+  }
+  return "en"
 }
